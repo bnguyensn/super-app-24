eb0bbbc21878068578d1c0c4b14b7ce529446bff
Binh Nguyen <b.nguyen.sn@gmail.com>
2021-02-03 03:37:33 +0000

    add back width and height

--- components/ReplIFrame.js
+++ components/ReplIFrame.js
@@ -3,6 +3,8 @@ export default function ReplIframe() {
     <div className="w-full h-full relative overflow-hidden">
       <iframe
         className="absolute top-0 left-0 w-full h-full border-0"
+        width="100%"
+        height="100%"
         title="Chat history"
         src="https://blog.repl.it/__logs"
         sandbox
